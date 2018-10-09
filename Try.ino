dc11253cb64319cd140c9d5bada82bd9a3ca3a8e
eNigma <miguel_on_94@hotmail.com>
2018-10-10 00:30:41 +0200

    Máquina de estados leer botones

--- Try.ino
+++ Try.ino
@@ -2,40 +2,43 @@
 #import <Wire.h>
 
 //Define variables
-#define clkpin	11
-#define	clkinh	5
-#define	serial	3
-#define shload	6
+int clkpin=4;
+int clkinh=5;
+int	serial=3;
+int	shload = 6;
 
 void (*machineList[4])();
 byte colums = 0;
 
 //Function definitions for ReadButtons FMM
 void nothing() {
-	pinMode(shload, LOW);
+	digitalWrite(clkinh, HIGH);
+	digitalWrite(shload, LOW);
 	machineList[0] = &LD;
 }
 
 void LD() {
-	pinMode(shload, HIGH);
+	delayMicroseconds(5);
+	digitalWrite(shload, HIGH);
 	machineList[0] = &SH;
 }
 
 void SH() {
-	pinMode(clkinh, LOW);
-	machineList[0] = &INH;
-}
-
-void INH(){
+	digitalWrite(clkinh, LOW);
 	machineList[0] = &read;
 }
 
 void read() {
 	colums = 0;
+
 	for (int i = 0; i < 8; i++) {
 		colums <<= 1;
 		colums |= digitalRead(serial);
+		digitalWrite(clkpin, HIGH);
+		delayMicroseconds(5);
+		digitalWrite(clkpin, LOW);
 	}
+	digitalWrite(clkinh, HIGH);
 	machineList[0] = &nothing;
 }
 
@@ -46,14 +49,12 @@ void setup()
 	machineList[0] = &nothing;
 	pinMode(clkpin, OUTPUT);
 	pinMode(clkinh, OUTPUT);
-	pinMode(serial, INPUT);
 	pinMode(shload, OUTPUT);
+	pinMode(serial, INPUT);
+
 	digitalWrite(clkinh, HIGH);
 	digitalWrite(shload, HIGH);
-
-	TCCR1A = bit(COM1A0);  
-	TCCR1B = bit(WGM12) | bit(CS10);  
-	OCR1A = 255;
+	digitalWrite(clkpin, LOW);
 }
 
 // Add the main program code into the continuous loop() function
